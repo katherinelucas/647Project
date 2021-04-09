fd3b1bd28b2052ed07d5e0aa8cc2dfe0f2c86f5e
Katherine Lucas <45476430+katherinelucas@users.noreply.github.com>
2021-04-09 17:46:44 -0500

    Update Animals.php

--- Animals.php
+++ Animals.php
@@ -1,1 +1,29 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+$mysqli = new mysqli("mysql.eecs.ku.edu", "katielucas", "Pairaiy9",
+"katielucas");
+/* check connection */
+if ($mysqli->connect_errno)
+{
+ printf("Connect failed: %s\n", $mysqli->connect_error);
+ exit();
+}
+$username = $_POST["username"];
+$result = mysqli_query($mysqli,"SELECT * FROM Animals");
 
+echo '<table border=\"1\">';
+while($row = mysqli_fetch_array($result))
+{
+    echo "<tr>";
+    echo "<td>".$row['content']."</td>";
+    echo "</tr>";
+}
+echo '</table>';
+
+ /* free result set */
+ $result->free();
+}
+/* close connection */
+$mysqli->close();
+?>
